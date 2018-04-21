042925bf0edff8bf6474a85a810aa667a8aaa890
Lukas Bohlmann <luk_boh@uni-bremen.de>
2018-04-21 12:03:23 +0200

    New version of seat recognizer

--- hardware/seat_cam/easyrider_seatcam.py
+++ hardware/seat_cam/easyrider_seatcam.py
@@ -1,34 +1,8 @@
-import sys
-sys.path.insert(0, 'pixy_build/libpixyusb_swig/')
+import detector
 
-import time
-from pixy import *
-from ctypes import *
+print ("Free Rider Seat Cam started!")
 
-# Pixy Python SWIG get blocks example #
+detector.init()
 
-print ("Pixy Python SWIG Example -- Get Blocks")
-
-# Initialize Pixy Interpreter thread #
-pixy_init()
-time.sleep(0.5)
-
-class Blocks (Structure):
-  _fields_ = [ ("type", c_uint),
-               ("signature", c_uint),
-               ("x", c_uint),
-               ("y", c_uint),
-               ("width", c_uint),
-               ("height", c_uint),
-               ("angle", c_uint) ]
-
-
-while(1):
-	if(pixy_blocks_are_new() == 1):
-		blocks = BlockArray(100)
-		count = pixy_get_blocks(100, blocks)
-		print 'Found %d blocks' % count
-		if count > 0:
-			for index in range (0, count):
-    				print '[BLOCK_TYPE=%d ANGLE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[index].type, blocks[index].angle, blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height)
-
+for i in range(0, 10):
+  print(detector.getCurrentOccupancy())
