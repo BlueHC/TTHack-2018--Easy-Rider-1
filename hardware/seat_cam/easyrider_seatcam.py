e8ed866f478fafd97143c25befc72502cbf4cc27
Lukas Bohlmann <lukasbohlmann@googlemail.com>
2018-04-21 09:25:33 +0000

    Bugfix

--- hardware/seat_cam/easyrider_seatcam.py
+++ hardware/seat_cam/easyrider_seatcam.py
@@ -11,6 +11,7 @@ print ("Pixy Python SWIG Example -- Get Blocks")
 
 # Initialize Pixy Interpreter thread #
 pixy_init()
+time.sleep(0.5)
 
 class Blocks (Structure):
   _fields_ = [ ("type", c_uint),
@@ -22,12 +23,12 @@ class Blocks (Structure):
                ("angle", c_uint) ]
 
 
-for i in range(0, 5):
-	time.sleep(3)
-	blocks = BlockArray(100)
-	count = pixy_get_blocks(100, blocks)
-	print 'Found %d blocks' % count
-	if count > 0:
-		for index in range (0, count):
-    			print '[BLOCK_TYPE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[index].type, blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height)
+while(1):
+	if(pixy_blocks_are_new() == 1):
+		blocks = BlockArray(100)
+		count = pixy_get_blocks(100, blocks)
+		print 'Found %d blocks' % count
+		if count > 0:
+			for index in range (0, count):
+    				print '[BLOCK_TYPE=%d ANGLE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[index].type, blocks[index].angle, blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height)
 
