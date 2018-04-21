fe3df3256f67090e06c54b1676ff840aab163de3
Lukas Bohlmann <lukasbohlmann@googlemail.com>
2018-04-21 11:57:45 +0000

    Optimizes seat recognition

--- hardware/seat_cam/detector.py
+++ hardware/seat_cam/detector.py
@@ -14,20 +14,21 @@ class Blocks (Structure):
                ("height", c_uint),
                ("angle", c_uint) ]
 
-freeSeats = []
-
 def init():
     pixy_init()
     time.sleep(0.5)
 
 def getCurrentOccupancy():
+    global freeSeats 
+    freeSeats = []
     blocks = BlockArray(100)
-    for i in range(0, 100):
-		count = pixy_get_blocks(100, blocks)
+    for i in range(0, 50000):
+	count = pixy_get_blocks(100, blocks)
         if(count>0):
             for index in range (0, count):
                 print '[BLOCK_TYPE=%d ANGLE=%d SIG=%d X=%3d Y=%3d WIDTH=%3d HEIGHT=%3d]' % (blocks[index].type, blocks[index].angle, blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height)
-    return freeSeats
+    		addBlock(blocks[index].x, blocks[index].y, blocks[index].angle)
+	return freeSeats
 
 def addBlock(x, y, angle):
     freeSeats.append([x, y, angle])
